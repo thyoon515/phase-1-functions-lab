594b2499ba07697e48c467c74dad9dcf6c4bce30
thyoon515 <thyoon515@gmail.com>
2022-04-07 23:52:36 -0400

    commit tree 62351fb364e1ea3b5fd86d0f18bd8847905d30d8

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+        return Math.abs(42 - someValue);
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination)*264;
+}
+
+function calculatesFarePrice(start, destination) {
+    
+    const distance = (Math.abs(start - destination)*264)
+    
+    if (distance <= 400) { 
+        console.log("free sample.")
+        return 0
+    }
+    else if((distance > 400) && (distance <= 2000)) {
+        return (distance - 400)*0.02;
+    }
+    else if ((distance > 2000) && (distance <= 2500)) {
+        return 25;
+    }
+    else if (distance > 2500) {
+        return "cannot travel that far";
+    }
+    }
+    
+    
